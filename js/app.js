c6f901327660f3eb739e814f458f234ebd43398e
juanmcr06 <juanmcr.06@gmail.com>
2022-09-24 05:42:25 -0300

    update

--- js/app.js
+++ js/app.js
@@ -17,3 +17,22 @@ enlaces[0].href = 'https://google.com';
 
 const heading2 = document.getElementById('heading');
 console.log(heading2);
+
+// Generar un nuevo enlace
+
+const nuevoEnlace = document.createElement('A');
+
+// Agregar el href
+
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+// Agregar texto
+
+nuevoEnlace.textContent = 'Un Nuevo Enlace';
+
+// Agregar una clase
+
+nuevoEnlace.classList.add('navegacion__enlace');
+
+
+console.log(nuevoEnlace);
